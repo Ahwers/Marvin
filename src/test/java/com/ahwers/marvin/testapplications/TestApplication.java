47466230b014e93daac8c1878f257f6a69f879c1
Josh Pannell <joshjo_p@live.co.uk>
2022-02-07 19:06:05 +0000

    Quick commit to clean up my workspace.

--- src/test/java/com/ahwers/marvin/testapplications/TestApplication.java
+++ src/test/java/com/ahwers/marvin/testapplications/TestApplication.java
@@ -1,13 +1,9 @@
 package com.ahwers.marvin.testapplications;
 
-import java.util.Map;
-
 import com.ahwers.marvin.framework.application.Application;
-import com.ahwers.marvin.framework.application.action.annotations.CommandMatch;
 import com.ahwers.marvin.framework.application.annotations.IntegratesApplication;
 import com.ahwers.marvin.framework.application.annotations.Stateful;
 import com.ahwers.marvin.framework.application.state.TestApplicationState;
-import com.ahwers.marvin.framework.resource.MarvinApplicationResource;
 
 @IntegratesApplication("Test Application")
 @Stateful(TestApplicationState.class)
